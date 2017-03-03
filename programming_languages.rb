d76777d849cbbee526c8e88ad56787cb67961fa4
Sammy Steiner <sammysteiner@gmail.com>
2017-03-03 21:25:06 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
   # your code here
+  my_hash = {}
+  languages[:oo].each do |key, value|
+    my_hash[key] = value
+    my_hash[key].merge!({:style => [:oo]})
+  end
+  languages[:functional].each do |key, value|
+    my_hash[key] = value
+    my_hash[key].merge!({:style => [:functional]})
+  end
+  my_hash[:javascript][:style] << :oo
+  languages = my_hash
 end
