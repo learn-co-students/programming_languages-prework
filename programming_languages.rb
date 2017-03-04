f2158e0189c5aeec0a322b3a104c1a239d06d928
Tim Freeman <d.timothy.freeman@gmail.com>
2017-03-04 17:02:41 -0500

    Programming Languages Prework -- Passed Tests

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,35 +1,3 @@
-languages = {
-      :oo => {
-        :ruby => {
-          :type => "interpreted"
-        },
-        :javascript => {
-          :type => "interpreted"
-        },
-        :python => {
-          :type => "interpreted"
-        },
-        :java => {
-          :type => "compiled"
-        }
-      },
-      :functional => {
-        :clojure => {
-          :type => "compiled"
-        },
-        :erlang => {
-          :type => "compiled"
-        },
-        :scala => {
-          :type => "compiled"
-        },
-        :javascript => {
-          :type => "interpreted"
-        }
-     
-      }
-    }
-
 def reformat_languages(languages)
 	new_hash = {}
 	languages.each do |style, language_name|
@@ -52,5 +20,3 @@ def reformat_languages(languages)
 	end
 	new_hash
 end
-
-reformat_languages(languages)
