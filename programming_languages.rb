03c75e7f4084cc43676cef303f9082429d222f08
Tim Freeman <d.timothy.freeman@gmail.com>
2017-03-04 17:01:11 -0500

    Programming Languages Prework -- Passed Tests

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,56 @@
+languages = {
+      :oo => {
+        :ruby => {
+          :type => "interpreted"
+        },
+        :javascript => {
+          :type => "interpreted"
+        },
+        :python => {
+          :type => "interpreted"
+        },
+        :java => {
+          :type => "compiled"
+        }
+      },
+      :functional => {
+        :clojure => {
+          :type => "compiled"
+        },
+        :erlang => {
+          :type => "compiled"
+        },
+        :scala => {
+          :type => "compiled"
+        },
+        :javascript => {
+          :type => "interpreted"
+        }
+     
+      }
+    }
+
 def reformat_languages(languages)
-  # your code here
+	new_hash = {}
+	languages.each do |style, language_name|
+		language_name.each do |name, type_key|
+			new_hash[name.to_sym] = {}
+		end
+		language_name.each do |name, type_key|
+			type_key.each do |type_key, type_string|
+				new_hash[name.to_sym][type_key] = type_string
+				new_hash[name.to_sym][:style] = []
+			end
+		end
+	end
+	languages.each do |style, language_name|
+		language_name.each do |name, type|
+			if new_hash.any? {|k,v| k == name}
+				new_hash[name][:style] << style
+			end
+		end
+	end
+	new_hash
 end
+
+reformat_languages(languages)
