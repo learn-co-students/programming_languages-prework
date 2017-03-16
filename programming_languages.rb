98645111484cf4591825fe83bea9fbc405813943
Marianna Katsika <marianna.katsika@gmail.com>
2017-03-16 21:40:34 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
+
 def reformat_languages(languages)
-  # your code here
+new_hash = { }
+languages.each do |styles, data|
+  data.each do |lang, types| 
+    types.each do |type, value|
+    if new_hash[lang].nil?
+    new_hash[lang] = {}
+    end
+    # I need some help with this one.
+    new_hash[lang][:style] ||= []
+    new_hash[lang][:style] << styles
+    if new_hash[lang][type].nil?
+      new_hash[lang][type] = value
+    end
+   end
+  end
+ end
+new_hash
 end
