bbbfd19636bdcf14078406d8352fa043adfcfda2
Mike Merin <mikemerinweather@gmail.com>
2017-03-19 02:55:49 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,32 @@
-def reformat_languages(languages)
-  # your code here
-end
+languages = {
+  :oo => {
+    :ruby => { :type => "interpreted"  },
+    :javascript => { :type => "interpreted" },
+    :python => { :type => "interpreted" },
+    :java => { :type => "compiled" }
+          },
+  :functional => {
+    :clojure => { :type => "compiled" },
+    :erlang => { :type => "compiled" },
+    :scala => { :type => "compiled" },
+    :javascript => { :type => "interpreted" }
+    } }
+
+    def reformat_languages(languages)
+    new_hash = {}
+    languages.each do |style, lang|
+      lang.each { |l,type| new_hash[l] = {type: "", style: [] } }
+    end
+    languages.each do |style, lang|
+      lang.each do |l,type|
+        type.each do |t, s|
+          new_hash[l][:type] = s
+          new_hash[l][:style] << style
+        end
+      end
+    end
+    new_hash
+  end
+
+
+p reformat_languages(languages)
---
+++ temp.rb
@@ -0,0 +1,29 @@
+languages = {
+  :oo => {
+    :ruby => { :type => "interpreted"  },
+    :javascript => { :type => "interpreted" },
+    :python => { :type => "interpreted" },
+    :java => { :type => "compiled" }
+          },
+  :functional => {
+    :clojure => { :type => "compiled" },
+    :erlang => { :type => "compiled" },
+    :scala => { :type => "compiled" },
+    :javascript => { :type => "interpreted" }
+    } }
+
+{
+  :ruby => { :type => "interpreted",
+             :style => [:oo] },
+  :javascript => { :type => "interpreted",
+             :style => [:oo, :functional] },
+  :python => { :type => "interpreted",
+               :style => [:oo] },
+  :java => { :type => "compiled",
+             :style => [:oo] },
+  :clojure => { :type => "compiled",
+                :style => [:functional] },
+  :erlang => { :type => "compiled",
+               :style => [:functional] },
+  :scala => { :type => "compiled",
+              :style => [:functional] } }
