ba7d7bb535bacd05e5b99fb875ca955bcfe5a344
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-03-25 11:38:35 -0400

    Reformatted

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,47 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, name|
+    name.each do |lan_name, type|
+      if new_hash[lan_name] == nil
+        new_hash[lan_name] = type
+        new_hash[lan_name][:style] = []
+      end
+      if new_hash[lan_name] != nil
+        new_hash[lan_name][:style] << style
+      end
+    end
+  end
+  new_hash
 end
+
+reformat_languages({
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+})
