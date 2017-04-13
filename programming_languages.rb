c93e6702707254e0dcfab49bf3a064da3f77e867
Alexander Boring <alexboring@hotmail.com>
2017-04-13 18:45:43 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require 'byebug'
+
 def reformat_languages(languages)
+  hash = {}
+  languages.each do |k, language|
+    language.each do |lang, type|
+      hash[lang] ||= {}
+      hash[lang][:style] ? hash[lang][:style] << k : hash[lang][:style] = [k]
+      hash[lang][:type] ? next : hash[lang][:type] = type[:type]
+    end
+  end
+
+  hash
+
   # your code here
 end
