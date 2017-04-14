99f446526e6c1ff6e230ed668371b47f5ab9b2f4
drzly777 <andrew.zheng725@gmail.com>
2017-04-14 13:02:40 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
   # your code here
+  new_format = {}
+  languages.each { |style,langs|
+    langs.each{ |lang, atts|
+      if new_format.keys.member?(lang)
+        new_format[lang][:style] << style
+      else
+        new_format[lang] = atts
+        new_format[lang][:style] = [style]
+      end
+    }
+  }
+  new_format
 end
