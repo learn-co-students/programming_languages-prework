0455b2cae11d563ccd408db312c28dd05e6b622f
github username <github email address>
2017-04-30 15:39:57 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,45 @@
+=begin
+  languages = {
+    :oo => {
+      :ruby => {
+        :type => "interpreted"
+      },
+      :javascript => {
+        :type => "interpreted"
+      },
+      :python => {
+        :type => "interpreted"
+      },
+      :java => {
+        :type => "compiled"
+      }
+    },
+    :functional => {
+      :clojure => {
+        :type => "compiled"
+      },
+      :erlang => {
+        :type => "compiled"
+      },
+      :scala => {
+        :type => "compiled"
+      },
+      :javascript => {
+        :type => "interpreted"
+      }
+
+    }
+  }
+=end
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |type, language_hash|
+    language_hash.each do |language, types|
+      new_hash[language] ||= types
+      new_hash[language][:style] ||= []
+      new_hash[language][:style] << type
+    end
+  end
+  new_hash
 end
