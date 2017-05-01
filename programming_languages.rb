796a75816c067ed2deadce1bb48537b4c1d7a84b
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-05-01 00:36:36 -0500

    binding.pry still doesn't work for me :(

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
 def reformat_languages(languages)
-  # your code here
+  hash_to_return = {}
+  languages.each do |type, language|
+    language.each do |name, attribute|
+      attribute.each do |key, value|
+        if hash_to_return[name].nil?
+          hash_to_return[name] = {}
+        end
+        #I unfortunately had to cheat with this next line
+        #becuase I didn't know that ||= was a thing.
+        hash_to_return[name][:style] ||= []
+        hash_to_return[name][:style] << type
+        if hash_to_return[name][key].nil?
+          hash_to_return[name][key] = value
+        end
+      end
+    end
+  end
+  hash_to_return
 end
