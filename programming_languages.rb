3942ee87402f818076f99595ab342221be48277a
Yelstin Fernandes <yfernan2@u.rochester.edu>
2017-05-09 16:37:25 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
 def reformat_languages(languages)
-  # your code here
+
+  new_hash = {}
+
+  languages.each do |kind, data|
+    data.each do |language, type_data|
+      type_data.each do |type, interpretation|
+        if new_hash[language]
+          new_hash[language][:style] << kind
+        else
+          new_hash[language] = {type => interpretation, style: [kind]}
+        end
+      end
+    end
+  end
+
+  new_hash
+
 end
