e747843e12a43db90f97c29f49477b10ee591862
Jason  Rosso <jasonrosso@gmail.com>
2017-05-18 00:47:57 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
 def reformat_languages(languages)
-  # your code here
+  new_language_hash={}
+
+ languages.each do |language_orientation,language_name|
+   language_name.each do|language_name_out, type|
+    out_name =  language_name_out
+    t = type[:type]
+    orientation = language_orientation
+     new_language_hash[language_name_out]={type:t,style: [language_orientation]}
+  end
+end
+  return new_language_hash
 end
