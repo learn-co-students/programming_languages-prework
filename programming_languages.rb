6f9be9353eaef8fc8445f00c27ced11213a32e71
xxDOOMbox <shawwian@gmail.com>
2017-07-31 09:35:23 -0600

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,10 @@
 def reformat_languages(languages)
-  # your code here
+  formattedLanguages = Hash.new 
+  languages.each do |paradigm, lang|
+  	lang.each do |name, data|
+  		formattedLanguages[name] =  data if formattedLanguages[name] == nil
+  		formattedLanguages[name][:style] == nil ? formattedLanguages[name][:style] = [paradigm] : formattedLanguages[name][:style] << paradigm 
+  	end 
+  end
+  formattedLanguages 
 end
