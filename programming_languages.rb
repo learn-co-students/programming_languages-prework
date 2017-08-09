3e9eceacebf7b5f8c6a84c7e618b873ccc4ef4e1
cpan001 <github email address>
2017-08-09 15:54:40 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,38 +1,14 @@
 require 'pry'
 
-def languages
-  {
-  :oo => {
-    :ruby => {:type => "interpreted"},
-    :javascript => {:type => "interpreted"},
-    :python => {:type => "interpreted"},
-    :java => {:type => "compiled"}
-  },
-  :functional => {
-    :clojure => {:type => "compiled"},
-    :erlang => {:type => "compiled"},
-    :scala => {:type => "compiled"},
-    :javascript => {:type => "interpreted"}
-
-  }
-}
-end
-
-def language_hash
-  languages.fetch(:oo).merge(languages.fetch(:functional))
-end
-
 def reformat_languages(languages)
   # your code here
-  languages2 = language_hash
-  languages2.each { |lang, type|
-    if languages[:oo].include?(lang) && languages[:functional].include?(lang)
-      type[:style] = [:oo, :functional]
-    elsif languages[:oo].include?(lang)
-      type[:style] = [:oo]
-    elsif languages[:functional].include?(lang)
-      type[:style] = [:functional]
-    end
+  new_hash = {}
+  languages.each { |style, langs|
+    langs.each { |lang, att|
+      new_hash[lang] ||= att
+      new_hash[lang][:style] ||= []
+      new_hash[lang][:style] << style
+    }
   }
-  languages2
+  new_hash
 end
