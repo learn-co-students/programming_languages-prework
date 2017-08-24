e57fbd745af9af8d20d8615ccee032e5e418e067
Ayreal <aeh.herman@gmail.com>
2017-08-24 17:09:50 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, a|
+    a.each do |lang, type|
+      if !new_hash.has_key?(lang)  # IF undefined, create everything
+        new_hash[lang] = type
+        new_hash[lang][:style] = [style]
+      else                         # ELSE already defined, just shovel the style
+        new_hash[lang][:style] << style
+      end
+    end
+  end
+  new_hash
 end
