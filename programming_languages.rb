658ea7670f5bccfa7046e10a87cfc6448f81d532
github username <github email address>
2017-08-24 20:53:31 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,48 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  reformatted_languages = {}
+  languages.each_key { |language_style|
+    languages[language_style].each_pair { |language_name, language_attributes|
+      unless (reformatted_languages.has_key? language_name)
+        reformatted_languages[language_name] = language_attributes
+      end
+
+	  unless(reformatted_languages[language_name].has_key? :style)
+        reformatted_languages[language_name][:style] = [language_style]
+      else
+        reformatted_languages[language_name][:style].push(language_style)
+      end
+    }
+  }
+  return reformatted_languages
 end
