103f6357d75bff621f061892de28bb18ef0550c7
Dick Ward <dick@thedickward.com>
2017-09-16 21:36:15 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,8 +3,6 @@ def reformat_languages(language_hash)
   language_hash.each do |style, language|
     language.each do |name, type|
       type.each do |the_word_type, actual_type|
-
-
         if new_hash.include?(name) == false
           new_hash[name] = {the_word_type => actual_type, style: []}
         end
