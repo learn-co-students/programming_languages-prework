4a281a60f32a6510942e1830abcc4ee441ffbbcb
psarma89 <priyam.sarma@gmail.com>
2017-10-08 15:36:28 -0400

    Add programming_languages.rb solutions file

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  # styles = languages.keys
+  # types = languages.values
+  languages.each do |styles,types|
+    types.each do |type,value|
+      new_hash[type] = value
+      new_hash[type][:style] = []
+    end
+  end
+
+  languages.each do |styles,types|
+    types.each do |type,value|
+      new_hash[type][:style] << styles
+    end
+  end
+#    new_hash[type][:style] << styles.to_s
+
+  new_hash
 end
