749f42fac4f22acb32311b7b8cf7e6e0894c1c43
Zach Vary <zach.vary@gmail.com>
2018-01-07 23:47:07 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |orientation, language|
+    language.each do |language_name, info|
+      info.each do |type, ic|
+        if new_hash.key?(language_name) == true
+          new_hash[language_name][:style] << orientation
+        else
+          new_hash[language_name] = {type => ic, style: [orientation]}
+        end
+      end
+    end
+  end
+  new_hash
 end
