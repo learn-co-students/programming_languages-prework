39e193417885fc3b536c31d30adca762565ef229
Jason Lim <jayugc@gmail.com>
2018-01-12 21:07:46 +0000

    Done.

--- hash.rb
+++ hash.rb
@@ -29,20 +29,3 @@ languages = {
 
   }
 }
-
-def reformat_languages(languages)
-  new_hash = {}
-
-  languages.each do |style, language|
-    language.each do |language, type|
-      type.each do |type, value|
-        if new_hash[language] != nil
-          new_hash[language][:style] << style
-        else new_hash[language].nil?
-          new_hash[language] = {type: value, style: [style]}
-        end
-      end
-    end
-  end
-  new_hash
-end
