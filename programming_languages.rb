dc2e49b44a15458ac87d096d64fbf1005bb24776
Mohammad Khan <sparrowangels@gmail.com>
2018-01-17 22:51:58 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,24 @@
 def reformat_languages(languages)
-  # your code here
+  # puts "**********************#{languages}"
+  hash = {}
+  languages.each do |style, style_languages|
+    current_style = style
+    style_languages.each do |language, properties|
+      hash[language]=properties  if hash[language].nil?
+      hash[language][:style] = [] if hash[language][:style].nil?
+      hash[language][:style] << current_style
+      # puts "**********#{language}::#{current_style}"
+    end #style_languages
+  end #each languages
+  # puts "*****************************************#{hash[:javascript][:style]}"
+  hash
 end
+
+# functional =>{
+#   :javascript => {   :type => "interpreted"}
+# }
+
+# :javascript => {
+#   :type => "interpreted",
+#   :style => [:oo, :functional]
+# },
