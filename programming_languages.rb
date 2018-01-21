252c5ddd71e2bcc3126286a365664f450780a002
allinthemind66 <r.nilon92@gmail.com>
2018-01-20 20:52:59 -0500

    programming languages

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,71 @@
+require 'pry'
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  language_names_array = []
+
+  languages.each do |type, name|
+    language_names_array << name.keys
+  end
+
+  names = language_names_array.flatten.uniq
+
+  names.each do |language|
+    new_hash[language] = {}
+  end
+
+
+  new_hash.each do |language, data|
+    languages.each do |type, name|
+      if name.keys.include?(language)
+    data[:type] = languages[type][language][:type]
+      end
+    end
+  end
+
+new_hash.each do |language, data|
+  data[:style] = []
+
+  languages.each do |type, name|
+    if name.keys.include?(language)
+
+      data[:style] << type
+
+    end
+  end
+end
+
+  new_hash
 end
