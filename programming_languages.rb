cfa25f44ced39535319e894a4668e407dbb2fc1b
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-23 13:56:20 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  final_hash = {}
+  languages.each do |prgrm, styles|
+    styles.each do |language, type_hash|
+      if final_hash.keys.include?(language)
+        final_hash[language][:style].push(prgrm)
+      else
+        final_hash[language] = {:type => type_hash.values[0],
+                            :style => [prgrm]}
+      end
+    end
+  end
+
+  final_hash
+
 end
