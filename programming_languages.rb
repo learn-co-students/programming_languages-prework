101fbf3bcb6e2dc975c5095f39b23f2a44c72ce1
Helen Liutongco <liutongcoh@gmail.com>
2018-01-27 21:04:48 -0600

    Improved programming_languages.rb

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,11 +5,10 @@ def reformat_languages(languages)
     style_lang << style
     languages.each do |language, type_hash|
       if return_hash[language]
-        return_hash[language][:style] = [:oo, style_lang[0]]
-      else
+        style_lang = return_hash[language][:style] + style_lang
+      end
         type_hash[:style] = style_lang
         return_hash[language] = type_hash
-      end
     end
       style_lang = []
   end
