dc08b92ba1a46e3016accbc55652c2d403329a98
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-05 22:39:46 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each { |style, langs|
+    langs.each { |langName, attributes|
+      if new_hash[langName]
+         new_hash[langName][:style].push(style)
+      else
+        new_hash[langName] = attributes
+        new_hash[langName][:style] = [style]
+      end
+    }
+  }
+  new_hash
 end
