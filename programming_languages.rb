c15ce0fec8e6edef9ec03d77b8a0f429539dd1e7
Adam Cohn <adamcohn88@gmail.com>
2018-03-15 15:37:30 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
 def reformat_languages(languages)
-  # your code here
+	new_hash = {
+	}
+	languages.each do |key, data|
+		data.each do |language, type|
+			new_hash[language] = type.merge :style => []
+		end
+	end
+	new_hash.each do |new_language, new_type|
+		languages.each do |style, data|
+			data.each do |language, type|
+				if new_language == language
+					new_hash[language][:style] << style
+				end
+			end
+		end
+	end
+	new_hash
 end
+
