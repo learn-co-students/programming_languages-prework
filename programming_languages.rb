aaacd0771750e194db2b805b1831927cac02a37f
briecodes <hello@brie.work>
2018-03-19 12:33:40 -0400

    Bonus Method Created.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, list|
+    list.each do |language, data|
+      data.each do |key, value|
+        if new_hash[language] == nil
+          new_hash[language] = {key => value}
+          new_hash[language][:style] = [style]
+        else
+          new_hash[language][:style] << style
+        end
+      end
+    end
+  end
+  puts new_hash
+  return new_hash
 end
