5937758b50f9dfa4435fea52051440c626eb67fe
Mike C <mikecronin92@gmail.com>
2018-03-26 23:38:51 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,47 @@
+require 'pry'
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  new_lang = {}
+  languages.each do |oo_f,langs|
+    langs.each do |name,data|
+      new_lang[name] = {type: data[:type]} if new_lang.has_key?(name) == false
+      if new_lang[name].has_key?(:style)
+        new_lang[name][:style] << oo_f
+      else 
+        new_lang[name][:style] = [oo_f]
+      end
+    end
+  end
+  return new_lang
 end
