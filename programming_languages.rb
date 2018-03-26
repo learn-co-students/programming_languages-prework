af4a851fa870617f7b18e5d9479c3b46a48d5da1
Ryan O'Toole <otoolerj@gmail.com>
2018-03-26 23:59:16 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  h1 = languages[:oo]
+  h2 = languages[:functional]
+  new_hash = h1.merge(h2)
+  new_hash[:ruby][:style] = [:oo]
+  new_hash[:javascript][:style] = [:oo, :functional]
+  new_hash[:python][:style] = [:oo]
+  new_hash[:java][:style] = [:oo]
+  new_hash[:clojure][:style] = [:functional]
+  new_hash[:erlang][:style] = [:functional]
+  new_hash[:scala][:style] = [:functional]
+  new_hash
 end
