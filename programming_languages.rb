5f0316233867ab3808088893162a23a53eb0a409
jessebillard <jessebillard@mac.com>
2018-03-30 11:58:17 -0400

    Programming Languages Hash

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,44 @@
+
 def reformat_languages(languages)
-  # your code here
+    new_hash = {}
+    languages.each do |styles, style_hash|
+        style_hash.each do |language, language_hash|
+            new_hash[language] = language_hash
+                new_hash.each do |k, v|
+                    case k
+                        when :clojure
+                            v[:style] = [:functional]
+                        when :erlang
+                            v[:style] = [:functional]
+                        when :scala
+                            v[:style] = [:functional]
+                        when :javascript
+                            v[:style] = [:functional, :oo]
+                        when :ruby
+                            v[:style] = [:oo]
+                        when :java
+                            v[:style] = [:oo]
+                        when :python
+                            v[:style] = [:oo]
+                    end
+                end
+        end
+    end
+    new_hash
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
