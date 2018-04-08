6a63d2db97cb01baeca2461514105f2521e6234a
travisdock@gmail.com <travisdock@gmail.com>
2018-04-07 20:10:26 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, languages|
+    languages.each do |language, info|
+      new_hash[language] = info
+      new_hash[language][:style] = []
+    end
+  end
+  languages.each do |style, languages|
+    languages.each do |language, info|
+      new_hash[language][:style] << style
+    end
+  end
+  return new_hash
 end
