9c8535ea4b6f7c993d9179c7f344f3699e13e596
eckmLJE <lucas.eckman@gmail.com>
2018-04-13 10:23:20 -0400

    programming programming languages

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
-def reformat_languages(languages)
-  # your code here
+require 'pry'
+
+def reformat_languages(languages_hash)
+  new_hash = {}
+  languages_hash.each do |style, languages|
+    languages.each do |language, type|
+      type.each do |key, value|
+        new_hash[language] = {key => value, :style => []}
+      end
+    end
+  end
+  languages_hash.each do |style, languages|
+    languages.each do |language, type|
+        new_hash[language][:style].push(style)
+    end
+  end
+  new_hash
 end
