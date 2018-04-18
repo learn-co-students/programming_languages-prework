72fd8b8cb40e5a648e862792fef68b735843c253
Daiane Bushey <daianebushey@gmail.com>
2018-04-18 19:17:09 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,10 +2,19 @@ require 'pry'
 
 def reformat_languages(languages)
   new_hash = {}
-  languages.map do |style, lang|
-    new_hash = lang
+  languages.each do |style, lang|
+
+    lang.each do |l|
+      # laguage key / hash value containing type and style
+      # check if language already belongs to new_hash
+      # shove style to [style]
+      if !new_hash.key?(l[0])
+        new_hash[l[0]] = l[1]
+        new_hash[l[0]][:style] = [style]
+      else
+        new_hash[l[0]][:style] << style
+      end
+    end
   end
   new_hash
-
-  binding.pry
 end
