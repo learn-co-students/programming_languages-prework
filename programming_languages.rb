9386e79da2433cb63f4e8296cb3feb433f9ede60
jarretbryan <jarret.bryan@gmail.com>
2018-04-20 23:18:04 -0400

    built top level hashes. need to iterate downwards

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def reformat_languages(languages)
   # your code here
+  #build it downwards
+
+  new_hash = {}
+  languages.each do |kind,language_name|
+    language_name.each do |language, type|
+      new_hash[language] = {}
+    end
+  end
+  binding.pry
+  new_hash.each do |language|
+    #language[:type] = #each language type from the previous hash must go here
+  end
 end
