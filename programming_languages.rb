90e619725c047f8b5fae7117d18cd6c1fbe3b0ca
jarretbryan <jarret.bryan@gmail.com>
2018-04-21 17:15:38 -0400

    have the structure mostly down - need to add in bottom nested hash

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,11 +6,10 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |kind,language_name|
     language_name.each do |language, type|
-      new_hash[language] = {}
+      new_hash[language] = {}.merge(type)
+
     end
   end
   binding.pry
-  new_hash.each do |language|
-    #language[:type] = #each language type from the previous hash must go here
-  end
+  new_hash
 end
