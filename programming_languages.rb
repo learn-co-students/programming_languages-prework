4a3320d9198d3cc3b653858bf133e853265c0084
Ian Canino <ian.canino@gmail.com>
2018-04-22 21:29:30 -0400

    implemented reorganization method for the languages hash

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, style_type|
+    style_type.each do |language, type_hsh|
+      type = type_hsh[:type]
+      if !new_hash.has_key?(language)
+        new_hash[language] = {type: type, style: [style]}
+      else
+        new_hash[language][:style].push(style)
+      end
+    end
+  end
+  new_hash
 end
