21bf1de350ac6acb7fa9acfefac9876abebf4fdc
Anthony Chang <awchang12@gmail.com>
2018-05-09 21:41:01 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
-  # your code here
-end
+  # your code here	
+  new_hash = {}
+
+  languages.each do |style, langs|
+    langs.each do |language, type|
+      if new_hash[language]
+        new_hash[language][:style].push(style)
+      else
+        new_hash[language] = {
+        type: languages[style][language][:type],
+        style: [] << style
+        }
+      end
+    end
+  end
+  new_hash
+end	 
