c6a45ad9ca4b8ec1d4c8c8b8a6bb805b9e5dcff0
iwilliams83 <indirajamurath@gmail.com>
2018-05-21 19:49:02 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,22 @@
 def reformat_languages(languages)
   # your code here
+  reformatted = {}
+
+  languages.each do |style,language_hash|
+
+    language_hash.each do |language,type_hash|
+
+        if reformatted.include?(language)
+          arr = []
+          arr = reformatted[language][:style] << style
+        else
+        type_hash.each do |k,v|
+          arr = []
+          reformatted[language] ={k => v, :style => arr << style}
+        end
+      end
+    end
+  end
+
+  return reformatted
 end
