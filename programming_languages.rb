4a64ec29ed9fb4f290d3c20eeb7b6c598c03ab66
Jesse McReady <jessemcready@gmail.com>
2018-06-26 22:51:42 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,22 @@
 def reformat_languages(languages)
+  new_hash = {}
   # your code here
+  languages.each do |paradigm, data|
+    data.each do |language, type|
+      new_hash[language] = {
+        type: type[:type],
+        style: []
+      }
+      #new_hash[language][:style] = []
+      #new_hash[language][:style] << paradigm
+    end
+  end
+  
+  languages.each do |paradigm, data|
+    data.each do |language, type|
+      new_hash[language][:style] << paradigm
+    end
+  end
+  
+  new_hash
 end
