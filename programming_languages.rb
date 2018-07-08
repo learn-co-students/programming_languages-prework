20ef63864828fb01f2a222a00a38b7fe806bcb08
Scarlett Perry <si.perry15@gmail.com>
2018-07-08 22:54:29 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
-  # your code here
+reformatted = {}
+
+  languages.each do |function, lang|
+    lang.each do |idioma, data|
+      if reformatted[idioma] == nil
+        reformatted[idioma] = data
+      end
+
+      if reformatted[idioma][:style] == nil
+        reformatted[idioma][:style] = [function]
+      else
+        reformatted[idioma][:style].push(function)
+      end
+    end
+  end 
+  reformatted
 end
