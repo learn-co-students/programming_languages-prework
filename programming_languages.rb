e808cbfa392d2cd0cab4ca8ae819158e485996f4
github username <github email address>
2018-07-09 12:17:26 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,34 @@
+# require 'pry'
+
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  style_str = ""
+  languages.each do |style, language|
+    # puts "#{style}: #{language}"
+    style_str = style
+    # binding.pry
+    language.each do |language, type|
+      new_hash[language] = {type: type}
+      puts type
+      type.each do |type, value|
+        new_hash[language] = {style: [style_str]}
+
+        # puts style_str
+        #
+        # puts "beginning #{type}: #{value}"
+        # binding.pry
+
+        #
+        #
+        #
+        # puts "after #{attribute}: #{value}"
+        # puts style_str
+
+      end
+    end
+  end
+  puts new_hash
+
 end
