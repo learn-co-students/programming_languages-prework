b757d2e71130e3e1d8dfde6707baae055c8be3b8
Malcolm Leake <echelon1089@gmail.com>
2018-07-13 00:03:19 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,16 +1,15 @@
 require 'pry'
 def reformat_languages(languages)
-
-  new_hash = {}
-  
+new_hash = {}
   languages.each do |type_of_languages, names|
+    
+   names.each do |type, value|
+    
+   
   
-  end  
- 
- 
-  names.each do |type, value|
-
-
+  
+  
+   end
   end
   new_hash    
 end
