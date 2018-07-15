065964d5fd81e3d9400fce6d0ced3c94f960c4a6
Malcolm Leake <echelon1089@gmail.com>
2018-07-15 22:26:40 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,11 +4,12 @@ new_hash = {}
   languages.each do |type_of_languages, names|
     
    names.each do |type, value|
-    
-   
+     new_hash[type] ||= value
+    new_hash[type][:style] ||= []
+ new_hash[type][:style] << type_of_languages
+
   
-  
-  
+  #binding.pry
    end
   end
   new_hash    
