0a547fda96b69ae5f50cf87159f6c73ca1cb863e
whynotski <laurawynosky@gmail.com>
2018-07-17 09:45:44 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,58 @@
-def reformat_languages(languages)
-  # your code here
+require 'pry'
+# this returns a single hash with the key showing the language andthe vlue equal to a hash containing a string for the key and an array as the value
+def reformat_languages(language)
+  # your code here (*** needs an end***)
+
+  languages = {
+    :oo => {
+      :ruby => {
+        :type => "interpreted"
+      },
+      :javascript => {
+        :type => "interpreted"
+      },
+      :python => {
+        :type => "interpreted"
+      },
+      :java => {
+        :type => "compiled"
+      }
+    },
+    :functional => {
+      :clojure => {
+        :type => "compiled"
+      },
+      :erlang => {
+        :type => "compiled"
+      },
+      :scala => {
+        :type => "compiled"
+      },
+      :javascript => {
+        :type => "interpreted"
+      }
+    }
+  }
+  new_hash = {}
+
+  #GOAL = {key = languages_by_style: value = {type=string: style=[array]}}
+  #first iterate through the initial hash
+   languages.each do |style, items|
+     items.each do |name, info|
+       info.each do |type, value|
+
+          if new_hash.include?(name)
+            new_hash[name][:style]<< style
+          else
+            new_hash[name] = {
+              type=>value,
+              style: [] << style
+            }
+          end
+        end
+      end
+    end
+
+
+new_hash
 end
