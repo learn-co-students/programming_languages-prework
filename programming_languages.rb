0ab3b482d17f31a53004332684083a940d91c4ff
Kristopher Bullinger <kris.bullinger@gmx.com>
2018-07-17 17:02:20 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,18 +4,12 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language_hash|
     language_hash.each do |language, information|
-      new_hash[language] = information
-      new_hash[language][:style] = []
+      new_hash[language] = information if new_hash[language].nil?
+      new_hash[language][:style] ||= []
+      new_hash[language][:style] << style
     end
   end
-  new_hash.each do |language, information|
-    if languages[:oo].keys.include? language
-      new_hash[language][:style] << :oo
-    end
-    if languages[:functional].keys.include? language
-      new_hash[language][:style] << :functional
-    end
-  end
+  
   new_hash
   
 end
