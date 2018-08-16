be0c8a59194815dcc46094fe04c8ed505176c3f8
Ed Putans <edputans@gmail.com>
2018-08-16 21:22:47 +0100

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  hash = {}
+
+  languages.each do |styler,lingo|
+    lingo.each do |lingo,typea|
+      hash[lingo] ||= typea
+      hash[lingo][:style]=[]
+    end
+  end
+  languages.each do |styler,lingo|
+    lingo.each do |lingo,typea|
+    hash[lingo][:style] << styler
+  end
+end
+  return hash
 end
