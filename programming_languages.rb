6797aab8aa1558a8efafc87653a7e6c9e603f6af
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-18 01:25:10 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -37,7 +37,7 @@ def reformat_languages(languages)
   languages.each do |style_of_language_key, style_of_language_hash|
     style_of_language_hash.each do |language_name_key, language_attributes_hash|
       language_attributes_hash.each do |type_key, type_value|
-      if new_hash[language_name_key] == nil 
+       if new_hash[language_name_key] == nil 
          new_hash[language_name_key] = {type_key =>  type_value, :style => [style_of_language_key]}
        else  new_hash[language_name_key][:style] << style_of_language_key
        end    
