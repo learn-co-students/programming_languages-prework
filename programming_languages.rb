9eb59ba33c18df68e59ff90cefb84bd19ce8443a
Kim Pham <pham.kim.a@gmail.com>
2018-08-24 15:27:39 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  
+  languages.each do |style, language_hash|
+    language_hash.each do |lang, type_hash|
+      type_hash.each do|type, value|
+        if new_hash.has_key?(lang)
+          new_hash[lang][:style].push(style)
+        else
+          new_hash[lang] = {type => value, :style => [style]}
+        end
+      end
+    end
+  end
+
+  new_hash
 end
