02f54a9530559820acaab68f5be34e8e553f47c9
Rob Parker <into.the.parker@gmail.com>
2018-09-11 13:14:48 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
 def reformat_languages(languages)
-  # your code here
-end
+    new_hash = {}
+    languages.each do |key, value|
+      value.each do |i,k|
+        new_hash[i] = k
+        new_hash[i][:style] = [key]
+      end
+    end
+
+   new_hash[:javascript][:style] << :oo
+   new_hash
+  end  
