3028260fee0958263ea53e8f52ae188cff56f581
Asaf Davidov <adavidov@conncoll.edu>
2018-09-12 14:05:04 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |mode, mode_data|
+    style_data.each do |lang, type|
+      new_hash[lang] = {:style:
+      [mode]}
+    end
+  end
+  new_hash
 end
