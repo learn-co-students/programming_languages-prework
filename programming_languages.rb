8d4a39fbd8aea441624395f102afdd7560f093cc
github username <github email address>
2018-10-11 15:19:14 -0400

    message

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do | oriented, language_data |
+  	language_data.each do | language, elements |
+	  		if new_hash[language] == nil
+	  			new_hash[language] = elements
+	  			new_hash[language][:style]=[oriented]
+	  		else
+	  			new_hash[language][:style] << oriented
+	  		end
+  		end
+  	end  
+  new_hash
 end
