4feddc0731a37d074853c9747147197fa95cc067
jbharwood <josephblakeharwood@gmail.com>
2018-10-15 16:49:14 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |type, language|
+    language.each do |lang, desc|
+      new_hash[lang] = desc
+        if lang == :ruby || lang == :python || lang == :java
+          new_hash[lang][:style] = [:oo]
+        elsif lang == :javascript
+          new_hash[lang][:style] = [:oo, :functional]
+        else
+          new_hash[lang][:style] = [:functional]
+        end
+      end
+  end
+  new_hash
 end
