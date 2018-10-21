423d538fdf24999f4ef3520c2d6f60f8ca32123a
isaac <oseasisaac91@gmail.com>
2018-10-21 13:42:44 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,27 @@
+
 def reformat_languages(languages)
   # your code here
+  formatted_hash = {}
+
+  languages.each do |oo_functional, comp_lang|
+
+    comp_lang.each do |lang, lang_facts_key|
+
+      lang_facts_key.each do |lang_type, type_string|
+
+        if formatted_hash.include? (lang)
+      formatted_hash[lang][:style].push(oo_functional)
+
+
+        else formatted_hash[lang] = {
+      lang_type => type_string,
+      :style => [].push(oo_functional)
+     }
+      end
+
+
+      end
+    end
+  end
+  formatted_hash
 end
