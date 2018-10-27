d2d2a0436ffac9d4ec589d7e926271c5baac7e33
Vladimir Deryuzhenko <317110@gmail.com>
2018-10-27 14:55:37 -0400

    +

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_languages = {}
+  # binding.pry
+  languages.each do |style, languages|
+    languages.each do |lang_name, value|
+        # binding.pry
+      new_languages[lang_name] ||= value
+      new_languages[lang_name][:style] ||= []
+      new_languages[lang_name][:style] << style
+      # binding.pry
+    end
+  end
+  # binding.pry
+  new_languages
 end
