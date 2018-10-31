1293fe93b788f38ed816d80904242de66a16a259
IMH51 <ianmartinharrison@outlook.com>
2018-10-31 14:45:53 +0000

    all languages reformatted correctly passing all tests

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  languages_hash = {}
+  languages.each do |style, language_hashes|
+    language_hashes.each do |language, type|
+      type.each do |key, value|
+      languages_hash[language] = {style: [] ,type: value}
+      end
+    end
+  end
+  languages.each do |style, language_hashes|
+    language_hashes.each do |language, type|
+      languages_hash[language][:style] << style
+    end
+  end
+  languages_hash
 end
