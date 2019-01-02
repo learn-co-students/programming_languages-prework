367f1137401a625d33a636db6acade3696e94792
Ross Jones <rossmosis.jones@gmail.com>
2019-01-02 00:52:59 -0500

    modified hash through a method

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+
+  languages.each do |styles, language_name|
+    language_name.each do |name, type|
+      if new_hash.has_key?(name)
+        new_hash[name][:style] << styles
+      else
+        new_hash[name] = type
+        new_hash[name][:style] = [styles]
+      end
+    end
+  end
+  new_hash
 end
