6ae1e777f5d623b0b3dc3f871c062a703b70958e
github username <github email address>
2019-01-11 09:28:16 -0500

    Completed Programming Languages Lab

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+new_hash = {}
+oo_hash = {}
+functional_hash = {}
+
+
+languages.each do |styles, name|
+  functional_hash = languages[:functional]
+    functional_hash.each do |name, attributes|
+      attributes[:style] = [:functional]
+      functional_hash
+#binding.pry
+    end
+end
+
+  languages.each do |styles, name|
+    oo_hash = languages[:oo]
+      oo_hash.each do |name, attributes|
+        attributes[:style] = [:oo]
+        oo_hash
+#binding.pry
+      end
+  end
+
+new_hash = oo_hash.merge(functional_hash)
+new_hash[:javascript][:style] << :oo
+
+new_hash
+#binding.pry
+
 end
