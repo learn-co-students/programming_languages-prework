6ca5b1cf01323cf8f935868c8a2d0a736bf967eb
Carlos barrera <carlosbarrera@Carloss-MacBook-Air.local>
2019-04-05 20:51:03 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,30 @@
+  require "pry"
 def reformat_languages(languages)
-  # your code here
+  languages.each do |key, value|
+    if key == :oo
+      languages[:ruby] = languages.delete :oo
+      binding.pry
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#new_hash = languages
+#languages[:ruby] = languages.delete :oo
+#languages[:ruby] = {:type => "interpreted"}
+#languages[:ruby] = {:style => [:oo]}
