3751619343ebd7954654b0433ae835d168bb74e5
Robert Pedersen <rob@worldcrimeleague.org>
2019-05-07 12:43:57 -0400

    done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |lang_type, lang_details|
+    lang_details.each do |name,details|
+      if new_hash[name]
+        new_hash[name][:style] << lang_type
+      else
+        new_hash[name] = {
+          :type => details[:type],
+          :style => [lang_type]
+        }
+      end
+    end
+  end
+  return new_hash  # your code here
 end
