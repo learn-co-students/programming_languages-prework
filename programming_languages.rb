c4c218cd140f7d7d988474771fa110f73c275d07
Roysan Easo <easo.roysan@gmail.com>
2019-05-14 12:52:27 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,30 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+    x = []
+    y = []
+    languages.each do |style,info|
+        info.each do |language,details|
+            x.push(style)
+            y.push(language)
+        end
+    end
+    new_hash = {}
+    languages.each do |style,info|
+        info.each do |language,details|
+            new_hash[language] = details
+        end
+    end
+
+    new_hash.each do |language, details|
+        details[:style] = []
+        y.each do |info|
+            if language == info
+                needed_index = y.index(info)
+                details[:style] << x[needed_index]
+                y.delete_at(needed_index)
+                x.delete_at(needed_index)
+            end
+        end
+    end
+    new_hash
 end
