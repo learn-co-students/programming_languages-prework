be614b65ddd9c9bcd261d6a2226445e9c08ea9cb
Varvara Babkova <varvara.munday@gmail.com>
2019-06-10 18:42:20 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,52 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |style, style_values|
+    style_values.each do |lang, lang_info|
+      if !new_hash.include?(lang)
+        new_hash[lang] = {
+           :type => lang_info[:type],
+           :style => [style]
+         }
+      else
+         new_hash[lang][:style].push (style)
+      end
+
+    end
+  end
+  puts new_hash
+
+  return new_hash
 end
+
+reformat_languages ({
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+})
