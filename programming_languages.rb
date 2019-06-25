ebbf5e5868dde862873e96ba9c21f56f7202f886
brinda behal <brindab50@gmail.com>
2019-06-25 18:48:23 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,10 +5,16 @@ def reformat_languages(languages)
   
   languages.each do |style, name_hash|
     name_hash.each do |language, language_data|
-      new_hash[language] = language_data
-       new_hash[language][:style] = [style]
-    binding.pry
+      language_data.each do |type, value|
+      if new_hash.has_key?(language)
+        new_hash[language][:style] << style
+      else
+      new_hash[language] = {type: value, style: [] << style}
+     
+       
+      end   
+    end
   end
-  end
+end
   new_hash
 end
